e84a893d3f8891d92fbeb10f3d39147c16332340
DanielGomez <danielgomez@DanielGMacPro.local>
2021-08-17 16:30:34 -0500

    initial configuration

---
+++ src/test/java/com/java/springdatajpaapplication/SpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.springdatajpaapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
